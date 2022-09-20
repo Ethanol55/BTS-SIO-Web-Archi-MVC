75fcc29dc5885136397423a00f42ffde2b6b6bc8
Ethan <ethanbellaiche0@gmail.com>
2022-09-20 22:16:22 +0200

    derniere modification (header location)

--- src/Controller/Etudiant/EtudiantUpdateController.php
+++ src/Controller/Etudiant/EtudiantUpdateController.php
@@ -26,22 +26,21 @@ class EtudiantUpdateController implements ControllerInterface
             if (filter_var($this->post["email"], FILTER_VALIDATE_EMAIL)) {
 
                     $encryptedPasssword = password_hash($this->post["pass"], PASSWORD_DEFAULT, ['cost' => 10]); // cryptage du mot de passe avec un cout de 10
-
                     $etudiantModel->updateStudent($this->post["email"], $encryptedPasssword, $this->post["login"], $this->post["nom"], $this->post["prenom"], $this->id);
+                // Cette ligne fera en sorte que lorsque l'etudiant sera modifié, on retournera sur la page avec la liste des etudiants
+                        header('Location: /etudiant');
                 }
             else {
                 echo "L'email saisi n'est pas sous la bonne forme";
             }
-                }
-            }
+        }
+    }
 
 
     public function outputEvent()
     {
         // Obj connect Mysql -> Obj Questionnaire
         $etudiantModel = new EtudiantModel();
-        // Cette ligne fera en sorte que lorsque l'etudiant sera modifié, on retournera sur la page avec la liste des etudiants
-//        header('Location: /etudiant');
         // Si y a pas de GET alors j'affiche tout
         return TwigCore::getEnvironment()->render(
             'etudiant/updateEtudiant.html.twig',
